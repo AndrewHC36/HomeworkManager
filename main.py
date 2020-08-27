a7bad9b7ab53e447b0f3b7c952e5c2b35626fa1a
Andrew Shen <andrewshen31@gmail.com>
2020-08-26 21:31:51 -0700

    Added static homeworks

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ E01_INV_CMD = "Invalid/Unkown Command"
 
 print(
 	"*****************************\n"
-	"*         The Super         |\n"
-	"*  Homework and Assigments  |\n"
-	"*          Manager          |\n"
+	"|         The Super         |\n"
+	"|  Homework and Assigments  |\n"
+	"|          Manager          |\n"
 	"*****************************\n"
 )
 
@@ -45,7 +45,7 @@ while True:
 				if cur_sbj != Null:
 					if cur_sbj in sbj:
 						homework = " ".join(tkn[2:])
-						todo.append((str(date.today()), hw_id, cur_sbj, homework))
+						todo.append((str(date.today()), hw_id, cur_sbj, homework, False))
 						hw_id += 1
 					else:
 						print("Invalid Subject")
@@ -53,6 +53,71 @@ while True:
 					print("Empty Subject")
 			else:
 				print("Empty Value")
+		else:
+			print(E01_INV_CMD)
+	elif check(0) == "EDIT":
+		if check(1) == "SUBJECT":  # edit subject <target name> <new name>
+			if check(2) != Null:
+				if sbj.count(check(2)) > 0:
+					if check(3) != Null:
+						sbj[sbj.index(check(2))] = check(3)
+					else:
+						print("Subject new name not found.")
+				else:
+					print("Subject not found.")
+			else:
+				print("Empty subject name")
+		elif check(1) == "HW":  # edit hw <homework id> <new name>
+			if check(2) != Null:
+				try:
+					hw_id = int(check(2))
+				except:
+					print("This is not a valid homework id")
+				else:
+					ids = [i for i in map(lambda x: x[1], todo)]
+					if ids.count(hw_id) > 0:
+						ind = ids.index(id_no)
+						if check(3) != Null:
+							todo[ind][3] = check(3)
+						else:
+							print("Empty new name for the homework")
+					else:
+						print("Homework ID not found.")
+			else:
+				print("Empty homework ID.")
+		else:
+			print(E01_INV_CMD)
+	elif check(0) == "SET":
+		if check(1) == "STATIC":  # set static <hw id>
+			if check(2) != Null:
+				try:
+					hw_id = int(check(2))
+				except:
+					print("This is not a valid homework id")
+				else:
+					ids = [i for i in map(lambda x: x[1], todo)]
+					if ids.count(hw_id) > 0:
+						ind = ids.index(hw_id)
+						todo[ind][4] = True
+					else:
+						print("Homework ID not found.")
+			else:
+				print("Empty homework ID.")
+		elif check(1) == "UNSTATIC":  # set unstatic <hw id>
+			if check(2) != Null:
+				try:
+					hw_id = int(check(2))
+				except:
+					print("This is not a valid homework id")
+				else:
+					ids = [i for i in map(lambda x: x[1], todo)]
+					if ids.count(hw_id) > 0:
+						ind = ids.index(hw_id)
+						todo[ind][4] = False
+					else:
+						print("Homework ID not found.")
+			else:
+				print("Empty homework ID.")
 		else:
 			print(E01_INV_CMD)
 	elif check(0) == "INTO":
@@ -64,7 +129,7 @@ while True:
 				print("Subject Not Found")
 		else:
 			print("Empty Value")
-	elif check(0) == "FINISHED":
+	elif check(0) == "FINISH":
 		if check(1) != Null:
 			if check(1).isdigit():
 				id_no = int(check(1))
@@ -95,16 +160,27 @@ while True:
 		else:
 			print("Empty Value")
 	elif check(0) == "TODO":
-		print("Date | Homework ID | Subject | Homework")
-		for i in todo:
-			print(i)
-	elif check(0) == "COMPLETED":
+		print("Date | Homework ID | Status | Subject | Homework")
+		# statics gets printed first regardless of date
+		for date, hw_id, subj, name, static in todo:
+			if static:
+				print(f"{date}{' '*(13-len(date))}{hw_id}{' '*(4-len(str(hw_id)))}{'STATIC'}{'  '}{subj}{' '*(2+len(max(sbj, key=lambda s: len(s)))-len(subj))}{name}")
+		for date, hw_id, subj, name, static in todo:
+			if not static:
+				print(f"{date}{' '*(13-len(date))}{hw_id}{' '*(4-len(str(hw_id)))}{'      '}{'  '}{subj}{' '*(2+len(max(sbj, key=lambda s: len(s)))-len(subj))}{name}")
+	elif check(0) == "FINISHED":
 		print("Date | Homework ID | Subject | Homework")
 		for i in compl:
 			print(i)
 	elif check(0) == "SAVE":
 		if check(1) != Null:
+			"""
+			Data Serialization (Save File) Version IDs
+			0 - Original one
+			1 - Added static state for homework
+			"""
 			data = {
+				"version": 1,
 				"subjects": sbj,
 				"todo": todo,
 				"completed": compl,
@@ -119,18 +195,47 @@ while True:
 			data = {}
 			with open(tkn[1], 'r') as fbj:
 				data = json.load(fbj)
-			sbj = data["subjects"]
-			todo = data["todo"]
-			compl = data["completed"]
-			hw_id = data["hw-id"]
+			if data["version"] == 0:  # version 0 does not support static, so it defaults to False
+				sbj = data["subjects"]
+				todo = [i for i in map(lambda x: [x[0], x[1], x[2], x[3], False], data["todo"])]
+				compl = [i for i in map(lambda x: [x[0], x[1], x[2], x[3], False], data["completed"])]
+				hw_id = data["hw-id"]
+			elif data["version"] == 1:
+				sbj = data["subjects"]
+				todo = data["todo"]
+				compl = data["completed"]
+				hw_id = data["hw-id"]
+			else:
+				print("Incompatable version")
 		else:
 			print("Empty Value")
 	elif check(0) == "INTERNAL":
 		print(f"Subjects: {sbj}")
 		print(f"TODO: {todo}")
-		print(f"COMPLETED: {compl}")
+		print(f"FINISHED: {compl}")
 		print(f"HW ID: {hw_id}")
 		print(f"Current Subj: {'<Null>' if cur_sbj == Null else cur_sbj}")
+	elif check(0) == "HELP":
+		print(
+			"""
+			******** Help Commands ********
+			ADD SUBJECT - To add subjects
+			ADD HW - To add homeworks under the specified subject
+			EDIT SUBJECT - Change the name of the subjects
+			EDIT HW - Change the assignment name of the homework
+			SET STATIC - Sets the homework status to static (the homework will be pinned and to signify for longterm/statuses/super long projects)
+			SET UNSTATIC - Sets the homework status to not static
+			INTO - Switches the specicified subject to that subject from this command
+			FINISH - Using the Homework ID parameter to move that Homework into the finished lists from the TODO lists
+			UNDO - Using the Homework ID parameter to move back the Homework from the finished lists to TODO lists
+			TODO - Shows the todo lists
+			FINISHED - Shows the finished lists
+			SAVE - Saves the current state to a file (JSON)
+			LOAD - Loads the save file (JSON)
+			INTERNAL - Displays the internal information for debugging purposes
+			HELP - Prints the information of a list of commands
+			"""
+		)
 	else:
 		print(E00_SYNTAX)
 	
@@ -159,7 +264,7 @@ Date Added | Homework ID | Subject    | Homework
 12/2/2019  | 2           | Math       | Lesson 1: pg. 45-47
 ```
 
-COMPLETE 1
+FINISH 1
 
 TODO
 ```
@@ -195,6 +300,11 @@ LOAD homework.data
 DELETE <Homework ID>  # this fully deletes the Homework into the Deleted database
 
 VIEW DELETE
+
+# This requires subject implied value too
+ADD EVENT Events No2
+
+
 
 """
 
